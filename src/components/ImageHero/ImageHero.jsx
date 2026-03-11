17212494d1829198c949c672f5aabd54bc97fd12
Hugo Curado <16318386+HFSVBC@users.noreply.github.com>
2026-03-10 22:24:35 -0400

    refactor image here for better performance

--- src/components/ImageHero/ImageHero.jsx
+++ src/components/ImageHero/ImageHero.jsx
@@ -12,7 +12,20 @@ const itemStyles = {
   display: 'flex',
   flexDirection: 'column',
   borderRadius: 0,
+  position: 'relative',
+  overflow: 'hidden',
 };
+
+const imgStyles = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  objectPosition: 'center',
+};
+
 const titleStyles = { background: '#ffffff', opacity: '80%', color: '#000000', p: '4px 8px', width: 'max-content' };
 const subtitleStyles = { ...titleStyles, color: '#940607' };
 
@@ -23,12 +36,12 @@ function ImageHero(props) {
     <Paper
       sx={{
         ...itemStyles,
-        background: `url("${item.image}") no-repeat center center`,
-        backgroundSize: 'cover',
         height: item.height ? item.height : itemStyles.height,
       }}
     >
-      <Box sx={{ flexGrow: 1 }}>
+      <Box component="img" src={item.image} alt={item.title} sx={imgStyles} />
+
+      <Box sx={{ flexGrow: 1, position: 'relative' }}>
         <Box>
           <Typography variant={onHero ? 'h1' : 'h4'} sx={titleStyles}>
             {item.title}
@@ -36,7 +49,7 @@ function ImageHero(props) {
         </Box>
       </Box>
 
-      <Box>
+      <Box sx={{ position: 'relative' }}>
         <Typography variant="h6" sx={subtitleStyles}>
           {item.subtitle1}
         </Typography>
